7cb49d27a56f13d5a91ed1238aef9fa149f6740d
Deodatuss <serbgan03@gmail.com>
2023-09-08 17:47:39 +0300

    save sticker set data to raw json file

--- src/save_deck.py
+++ src/save_deck.py
@@ -1,5 +1,5 @@
 """
-Bot that recieves a sticker, get JSON data on it's stickerset and stores 
+Helper bot that recieves a sticker, get JSON data on it's stickerset and stores 
 some of stickers' info in a data/deck.json file to be fetched during the game.
 """
 import os
@@ -18,48 +18,43 @@ from telegram.ext import (
 
 from get_token import get_token
 
-
-# Enable logging
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
-# set higher logging level for httpx to avoid all GET and POST requests being logged
-logging.getLogger("httpx").setLevel(logging.WARNING)
-
-logger = logging.getLogger(__name__)
+_SCRIPT_DIR = os.path.dirname(__file__)
+_RAW_JSON_STICKER_SET_PATH = "../tmp/raw_sticker_set.json"
 
 
-async def start() -> None:
-    pass
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(
+        "This bot will take any sticker and turn it's set into a deck"
+        " for the FallacyMania bot game. Send a sticker to begin."
+    )
 
 
 async def get_sticker(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(
-        """
-        Save the sticker set associated with this sticker as a deck?
-        \n If yes, send /save command and it will become a new deck. 
-        """
+        "Save the sticker set associated with this sticker as a deck?"
+        "\n If yes, send /save command and it will become a new deck."
     )
-    sticker: Sticker = update.message.sticker
-    # my_set: StickerSet = await context.bot.get_sticker_set(my_sticker.set_name)
-    # print(my_set.to_json)
-    context.user_data["last_sticker_id"] = sticker.file_id
-    context.user_data["last_sticker_set"] = sticker.set_name
+    context.user_data["last_sticker_set"] = update.message.sticker.set_name
 
 
 async def save(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    # update.message.reply_sticker(
-    #     update.effective_chat.id,
-    # )
-    if context.user_data.get("last_sticker_id"):
-        await update.message.reply_sticker(context.user_data.get("last_sticker_id"))
+    sticker_set_name = context.user_data.get("last_sticker_set")
+
+    if sticker_set_name:
+        sticker_set = await context.bot.get_sticker_set(sticker_set_name)
+        json_file_path = os.path.join(_SCRIPT_DIR, _RAW_JSON_STICKER_SET_PATH)
+
+        with open(json_file_path, "w") as json_file:
+            json_file.write(sticker_set.to_json())
+
+        await update.message.reply_text("Deck saved successfully.")
     else:
-        await update.message.reply_text("You haven't sent any sticker yet.")
+        await update.message.reply_text("You haven't send any sticker yet.")
 
 
 def main() -> None:
     """Run the bot."""
-    application = ApplicationBuilder.token(get_token()).build()
+    application = ApplicationBuilder().token(get_token()).build()
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("save", save))
