e14ad6a74b951f30ed1e245c483d20644953b918
Deodatuss <serbgan03@gmail.com>
2023-09-09 19:29:24 +0300

    add json files and stubs for users and players

--- src/constants.py
+++ src/constants.py
@@ -5,3 +5,7 @@ RAW_JSON_STICKERSET_PATH = os.path.join(
 )
 
 JSON_DECK_PATH = os.path.join(os.path.dirname(__file__), "../data/sticker_deck.json")
+
+JSON_PLAYERS_PATH = os.path.join(os.path.dirname(__file__), "../config/players.json")
+
+JSON_USERS_PATH = os.path.join(os.path.dirname(__file__), "../config/players.json")
