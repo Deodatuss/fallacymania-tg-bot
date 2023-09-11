bed4503ef57d72aa9abf67575ca2d14a3a33e812
Deodatuss <serbgan03@gmail.com>
2023-09-11 21:26:03 +0300

    split deck_builder logic into several functions

--- src/deck_json_builder.py
+++ src/deck_json_builder.py
@@ -12,16 +12,23 @@ _RAW_JSON_STICKERSET_PATH = constants.RAW_JSON_STICKERSET_PATH
 _JSON_DECK_PATH = constants.JSON_DECK_PATH
 
 
-# TODO: split into functions to only convert and only read/write files
-async def stickerset_to_deck_converter() -> None:
+async def _read_raw_stickerset_file() -> dict:
     """
-    Reads raw JSON file with sticker set info, trims most data from
-    'stickers' list except emoji, file_id, full file_size.
-    Saves trimmed data to another JSON file.
+    Reads raw JSON file with sticker set info.
+    Returns dict with raw data.
     """
-
     with open(_RAW_JSON_STICKERSET_PATH, "r") as file:
         raw_set_data: dict = json.load(file)
+    return raw_set_data
+
+
+async def _stickerset_to_deck_converter(raw_set_data) -> dict:
+    """
+    Takes raw_set_data as input, trims most data from
+    'stickers' list except emoji, unique_file_id, file_id and file_size.
+    Returns dict with a formatted deck.
+    """
+
     deck_data = dict.fromkeys(raw_set_data.keys(), {})
 
     # copy sticker set meta-info (without stickers)
@@ -37,12 +44,32 @@ async def stickerset_to_deck_converter() -> None:
             "file_size": sticker["file_size"],
         }
 
+    return deck_data
+
+
+async def read_deck_file() -> dict:
+    """
+    Reads sticker deck file directly, without conversion from raw stickerset.
+    Returns dict with a deck.
+    """
+    with open(_JSON_DECK_PATH, "r") as file:
+        deck_data: dict = json.load(file)
+
+    return deck_data
+
+
+async def _save_deck_to_file(deck_data: dict) -> None:
+    """
+    Saves deck to a dedicated game file.
+    """
     with open(_JSON_DECK_PATH, "w") as file:
         json.dump(deck_data, file)
 
 
 async def main():
-    await stickerset_to_deck_converter()
+    raw_data = await _read_raw_stickerset_file()
+    deck_data = await _stickerset_to_deck_converter(raw_data)
+    await _save_deck_to_file(deck_data)
 
 
 if __name__ == "__main__":
