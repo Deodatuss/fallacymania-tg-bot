f5ee4af93d67ed7c7080ef85072a4e0ac0c57876
Deodatuss <serbgan03@gmail.com>
2023-09-13 17:24:45 +0300

    add TypeDict and split entity_data into roles

--- src/entities.py
+++ src/entities.py
@@ -1,5 +1,7 @@
 import json
 
+from typing import TypedDict
+
 from telegram import Update
 
 import constants
@@ -7,6 +9,29 @@ import constants
 _USERS_FILE = constants.JSON_USERS_PATH
 _PLAYERS_FILE = constants.JSON_PLAYERS_PATH
 _JSON_DECK_PATH = constants.JSON_DECK_PATH
+_DEBATERS_DICT_KEY = constants.DEBATERS_DICT_KEY
+_GESSERS_DICT_KEY = constants.DEBATERS_DICT_KEY
+
+
+class Debater(TypedDict):
+    chat_id: int
+    full_name: str
+    username: str
+    language_code: str
+    hand: dict[str, int]
+
+
+class Points(TypedDict):
+    score: int
+    attempts: int
+
+
+class Guesser(TypedDict):
+    chat_id: int
+    full_name: str
+    username: str
+    language_code: str
+    points: Points
 
 
 def get_deck() -> dict:
@@ -17,57 +42,84 @@ def get_deck() -> dict:
     return data
 
 
-def entity_data(update: Update) -> dict:
+def get_debater(update: Update) -> dict:
     user = update.effective_user
+    hand = {}
 
-    return {
-        user.id: {
-            "chat_id": update.effective_chat.id,
-            "full_name": user.full_name,
-            "username": user.username,
-            "language_code": user.language_code,
-        }
+    data: Debater = {
+        "chat_id": update.effective_chat.id,
+        "full_name": user.full_name,
+        "username": user.username,
+        "language_code": user.language_code,
+        "hand": hand,
     }
 
-
-def add_general_user(update: Update) -> None:
-    """
-    Stores all users that interacted with bot in users.json
-    """
-    with open(_USERS_FILE, "r") as file:
-        data = json.load(file)
-
-    data["users"].append(entity_data(update))
-
-    with open(_USERS_FILE, "w") as file:
-        json.dump(data, file)
+    return {user.id: data}
 
 
-# not used: data stored as dict inside context.bot_data for I/O economy
-def _add_debater(update: Update) -> None:
-    """
-    Stores active session debaters in players.json
-    """
-    with open(_PLAYERS_FILE, "r") as file:
-        data = json.load(file)
+def get_guesser(update: Update) -> dict:
+    user = update.effective_user
+    points: Points = {"score": 0, "attempts": 0}
 
-    new_data = entity_data(update)
+    data: Guesser = {
+        "chat_id": update.effective_chat.id,
+        "full_name": user.full_name,
+        "username": user.username,
+        "language_code": user.language_code,
+        "points": points,
+    }
 
-    data["debaters"].append(new_data)
-
-    with open(_PLAYERS_FILE, "w") as file:
-        json.dump(data, file)
+    return {user.id: data}
 
 
-# not used: data stored as dict inside context.bot_data for I/O economy
-def _add_guesser(update: Update) -> None:
-    """
-    Stores active session guessers in players.json
-    """
-    with open(_PLAYERS_FILE, "r") as file:
-        data = json.load(file)
+def entity_data(bot_data_dict_key: str, update: Update) -> dict:
+    if bot_data_dict_key == _DEBATERS_DICT_KEY:
+        return get_debater(update)
+    elif bot_data_dict_key == _GESSERS_DICT_KEY:
+        return get_guesser(update)
+    raise KeyError
 
-    data["guessers"].append(entity_data(update))
 
-    with open(_PLAYERS_FILE, "w") as file:
-        json.dump(data, file)
+# Currently not used; entities are for program runtime only
+
+# def add_general_user(update: Update) -> None:
+#     """
+#     Stores all users that interacted with bot in users.json
+#     """
+#     with open(_USERS_FILE, "r") as file:
+#         data = json.load(file)
+
+#     data["users"].append(entity_data(update))
+
+#     with open(_USERS_FILE, "w") as file:
+#         json.dump(data, file)
+
+
+# # not used: data stored as dict inside context.bot_data for I/O economy
+# def _add_debater(update: Update) -> None:
+#     """
+#     Stores active session debaters in players.json
+#     """
+#     with open(_PLAYERS_FILE, "r") as file:
+#         data = json.load(file)
+
+#     new_data = entity_data(update)
+
+#     data["debaters"].append(new_data)
+
+#     with open(_PLAYERS_FILE, "w") as file:
+#         json.dump(data, file)
+
+
+# # not used: data stored as dict inside context.bot_data for I/O economy
+# def _add_guesser(update: Update) -> None:
+#     """
+#     Stores active session guessers in players.json
+#     """
+#     with open(_PLAYERS_FILE, "r") as file:
+#         data = json.load(file)
+
+#     data["guessers"].append(entity_data(update))
+
+#     with open(_PLAYERS_FILE, "w") as file:
+#         json.dump(data, file)
